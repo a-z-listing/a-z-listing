337edb93e64a69b2968b7dae5ada4e338706f1af
Daniel Llewellyn <daniel@bowlhat.net>
2016-12-14 06:19:30 +0000

    version 1.1.0
    minor refactoring to remove unused variables
    fix some code-smell (phpcs)
    update readme

--- partials/az-listing.php
+++ partials/az-listing.php
@@ -58,6 +58,7 @@ function num_a_z_letters() {
  * @deprecated use get_the_a_z_letter_count() or the_a_z_letter_count()
  */
 function num_a_z_posts() {
+	/** @noinspection PhpDeprecationInspection */
 	return num_a_z_items();
 }
 /**
@@ -145,65 +146,73 @@ function get_the_a_z_item_permalink( $query = null ) {
 
 /**
  * @deprecated use the_a_z_listing()
+ * @param null|string|WP_Query|A_Z_Listing $query
  */
-function the_az_listing( $query = null, $colcount = 1, $minpercol = 10, $h = 2 ) {
-	the_a_z_listing( $query, $colcount, $minpercol, $h );
+function the_az_listing( $query = null ) {
+	the_a_z_listing( $query );
 }
 /**
  * Print the A-Z Index page content.
  * @param array|string|WP_Query|A_Z_Listing  $query      a valid WordPress query or an A_Z_Listing instance
- * @param integer                            $colcount   number of columns to separate the list of posts into.
- * @param integer                            $minpercol  minimum number of posts in each column before starting a new column.
- * @param integer                            $h          Heading-Level number for the section headings. May be 1 thru 7.
  */
-function the_a_z_listing( $query = null, $colcount = 1, $minpercol = 10, $h = 2 ) {
-	echo get_the_a_z_listing( $query, $colcount, $minpercol, $h ); // WPCS: XSS OK.
+function the_a_z_listing( $query = null ) {
+	echo get_the_a_z_listing( $query ); // WPCS: XSS OK.
 }
 
 /**
  * @deprecated use get_the_a_z_listing()
+ * @param array|string|WP_Query|A_Z_Listing  $query      a valid WordPress query or an A_Z_Listing instance
+ * @return string
  */
-function get_the_az_listing( $query = null, $colcount = 1, $minpercol = 10, $h = 2 ) {
-	return get_the_a_z_listing( $query, $colcount, $minpercol, $h );
+function get_the_az_listing( $query = null ) {
+	return get_the_a_z_listing( $query );
 }
 /**
  * Return the index of posts ordered and segmented alphabetically.
  * @param  array|string|WP_Query|A_Z_Listing  $query      a valid WordPress query or an A_Z_Listing instance
- * @param  integer                            $colcount   number of columns to separate the list of posts into.
- * @param  integer                            $minpercol  minimum number of posts in each column before starting a new column.
- * @param  integer                            $h          Heading-Level number for the section headings. May be 1 thru 7.
  * @return string                                         The listing html content ready for echoing to the page.
  */
-function get_the_a_z_listing( $query = null, $colcount = 1, $minpercol = 10, $h = 2 ) {
+function get_the_a_z_listing( $query = null ) {
 	return a_z_listing_cache( $query )->get_the_listing();
 }
 
 /**
  * @deprecated use the_a_z_letters()
+ * @param array|string|WP_Query|A_Z_Listing  $query      a valid WordPress query or an A_Z_Listing instance
+ * @param bool|string $target URL of the page to send the browser when a letter is clicked.
+ * @param bool $styling
  */
 function the_az_letters( $query = null, $target = false, $styling = false ) {
 	the_a_z_letters( $query, $target, $styling );
 }
+
 /**
  * Prints the A-Z Letter list.
- * @param array|string|WP_Query|A_Z_Listing  $query   a valid WordPress query or an A_Z_Listing instance
- * @param string                             $target  URL of the page to send the browser when a letter is clicked.
+ * @param array|string|WP_Query|A_Z_Listing $query a valid WordPress query or an A_Z_Listing instance
+ * @param bool|string $target URL of the page to send the browser when a letter is clicked.
+ * @param bool $styling
  */
 function the_a_z_letters( $query = null, $target = false, $styling = false ) {
-	echo get_the_a_z_letters( $query, $target, $styling, $_a_z_listing_object ); // WPCS: XSS OK.
+	echo get_the_a_z_letters( $query, $target, $styling ); // WPCS: XSS OK.
 }
 
 /**
  * @deprecated use get_the_a_z_letters()
+ * @param array|string|WP_Query|A_Z_Listing $query a valid WordPress query or an A_Z_Listing instance
+ * @param bool|string $target URL of the page to send the browser when a letter is clicked.
+ * @param bool $styling
+ * @return string
  */
 function get_the_az_letters( $query = null, $target = false, $styling = false ) {
 	return get_the_a_z_letters( $query, $target, $styling );
 }
+
 /**
  * Returns the A-Z Letter list.
- * @param  array|string|WP_Query|A_Z_Listing  $query   a valid WordPress query or an A_Z_Listing instance
- * @param  string                             $target  URL of the page to send the browser when a letter is clicked.
- * @return string                                      HTML ready for echoing containing the list of A-Z letters with anchor links to the A-Z Index page.
+ * @param  array|string|WP_Query|A_Z_Listing $query a valid WordPress query or an A_Z_Listing instance
+ * @param bool|string $target URL of the page to send the browser when a letter is clicked.
+ * @param bool $styling
+ * @return string HTML ready for echoing containing the list of A-Z letters with anchor links to the A-Z Index page.
  */
 function get_the_a_z_letters( $query = null, $target = false, $styling = false ) {
 	return a_z_listing_cache( $query )->get_the_letters( $target, $styling );
