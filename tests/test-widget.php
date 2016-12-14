82667fc8537d4e3d3642d8e5e90fd9941260c3c3
Daniel Llewellyn <daniel@bowlhat.net>
2016-12-14 13:53:54 +0000

    tests for lowercased titles
    add testing to verify the fix for lowercase titles not showing.
    ref: https://wordpress.org/support/topic/lowercase-post-titles-2/#post-8550569

--- tests/test-widget.php
+++ tests/test-widget.php
@@ -43,4 +43,27 @@ class AZ_Widget_Tests extends WP_UnitTestCase {
 			)
 		);
 	}
+
+	function test_populated_widget_lowercase_titles() {
+		$p = $this->factory->post->create( array( 'post_title' => 'Index Page', 'post_type' => 'page' ) );
+		$p2 = $this->factory->post->create( array( 'post_title' => 'test post', 'post_type' => 'page' ) );
+
+		$expected = sprintf( file_get_contents( 'tests/populated-widget.txt' ), $p );
+		$expected = preg_replace( '/\s{2,}|\t|\n/', '', $expected );
+
+		$this->expectOutputString( $expected );
+
+		the_section_a_z_widget(
+			array(
+				'before_widget' => '<div>',
+				'after_widget' => '</div>',
+				'before_title' => '<h2>',
+				'after_title' => '</h2>',
+			),
+			array(
+				'title' => 'Test Widget',
+				'post' => $p,
+			)
+		);
+	}
 }
