17e28b56e2ccb487b18423ee6a6c3c07b5ff444f
Daniel Llewellyn <daniel@bowlhat.net>
2018-12-08 12:29:31 +0000

    update template

--- templates/a-z-listing.php
+++ templates/a-z-listing.php
@@ -2,8 +2,8 @@
 /**
  * Default multicolumn template for the A-Z Listing plugin
  *
- * This template will be given the variable `$a_z_query` which is an instance of
- * `A_Z_Listing`.
+ * This template will be given the variable `$a_z_query` which is an instance
+ * of `A_Z_Listing`.
  *
  * You can override this template by copying this file into your theme
  * directory.
@@ -11,7 +11,13 @@
  * @package a-z-listing
  */
 
-$_a_z_listing_colcount  = 3;
+/**
+ * This value indicates the number of posts to require before a second column
+ * is created. However, due to the design of web browsers, the posts will flow
+ * evenly between the available columns. E.g. if you have 11 items, a value of
+ * 10 here will create two columns with 6 items in the first column and 5 items
+ * in the second column.
+ */
 $_a_z_listing_minpercol = 10;
 ?>
 <div id="az-tabs">
@@ -29,27 +35,38 @@ $_a_z_listing_minpercol = 10;
 				?>
 				<?php if ( $a_z_query->have_items() ) : ?>
 					<?php
-					$item_count   = $a_z_query->get_the_letter_count();
-					$column_limit = round( $item_count / $_a_z_listing_minpercol );
-					if ( $column_limit > $_a_z_listing_colcount ) {
-						$column_limit = $_a_z_listing_colcount;
-					}
+					$item_count  = $a_z_query->get_the_letter_count();
+					$num_columns = ceil(
+						$item_count / $_a_z_listing_minpercol
+					);
 					?>
-					<div class="letter-section" id="<?php $a_z_query->the_letter_id(); ?>">
+					<div class="letter-section"
+						id="<?php $a_z_query->the_letter_id(); ?>">
 						<h2 class="letter-title">
-							<span><?php $a_z_query->the_letter_title(); ?></span>
+							<span>
+								<?php $a_z_query->the_letter_title(); ?>
+							</span>
 						</h2>
-						<ul class="columns max-<?php echo $column_limit; ?>-columns">
+						<?php $column_class = "max-$num_columns-columns"; ?>
+						<ul class="columns <?php echo $column_class; ?>">
 							<?php
 							while ( $a_z_query->have_items() ) :
 								$a_z_query->the_item();
 								?>
 								<li>
-									<a href="<?php $a_z_query->the_permalink(); ?>"><?php $a_z_query->the_title(); ?></a>
+									<a href="
+										<?php $a_z_query->the_permalink(); ?>
+									">
+										<?php $a_z_query->the_title(); ?>
+									</a>
 								</li>
 							<?php endwhile; ?>
 						</ul>
-						<div class="back-to-top"><a href="#letters"><?php _e( 'Back to top', 'a-z-listing' ); ?></a></div>
+						<div class="back-to-top">
+							<a href="#letters">
+								<?php _e( 'Back to top', 'a-z-listing' ); ?>
+							</a>
+						</div>
 					</div>
 					<?php
 				endif;
@@ -59,6 +76,13 @@ $_a_z_listing_minpercol = 10;
 	</div>
 </div>
 <?php else : ?>
-	<p><?php esc_html_e( 'There are no posts included in this index.', 'a-z-listing' ); ?></p>
+	<p>
+		<?php
+		esc_html_e(
+			'There are no posts included in this index.',
+			'a-z-listing'
+		);
+		?>
+	</p>
 	<?php
 endif;
