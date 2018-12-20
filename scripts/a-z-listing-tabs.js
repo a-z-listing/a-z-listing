70e140ba8859924f7ea57a0d2f155b075e4683e3
Daniel Llewellyn <daniel@bowlhat.net>
2018-12-20 17:14:04 +0000

    update tabs javascript
    attempt to display the correct letter when a user clicks the widget link on another page

--- scripts/a-z-listing-tabs.js
+++ scripts/a-z-listing-tabs.js
@@ -1,4 +1,18 @@
 // props to @claytonchase (https://profiles.wordpress.org/claytonchase)
 jQuery( document ).ready( function() {
-    jQuery( "#az-tabs" ).tabs();
+    const tabs = document.getElementById( 'az-tabs' );
+    if ( tabs ) {
+        let link;
+        const letters = tabs.getElementById( 'letters' );
+        if ( letters ) {
+            link = letters.querySelector( `a[href="${window.location.hash}"]` );
+        }
+
+        if ( window.location.hash && link ) {
+            const activeTab = jQuery( link ).parent().index();
+            jQuery( tabs ).tabs( { active: activeTab } );
+        } else {
+            jQuery( tabs ).tabs();
+        }
+    }
 } );
