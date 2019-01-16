702b73a705ebcea4bb26f97b2f02ad8e5379aead
Daniel Llewellyn <daniel@bowlhat.net>
2019-01-16 14:19:45 +0000

    bump version number

--- a-z-listing.php
+++ a-z-listing.php
@@ -7,7 +7,7 @@
  * Author URI:      https://bowlhat.net/
  * Text Domain:     a-z-listing
  * Domain Path:     /languages
- * Version:         2.1.2
+ * Version:         2.1.3
  *
  * @package         A_Z_Listing
  */
