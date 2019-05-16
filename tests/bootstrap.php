7f4b07f858dbbb03a87b607d2df25e9c714208a8
Daniel Llewellyn <daniel@bowlhat.net>
2019-05-16 18:30:11 +0100

    Update test bootstrap.php file
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,31 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package a-z-listing
+ */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
+	exit( 1 );
+}
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+// Load a-z-listing-specific test extension
+require 'assert-html.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/a-z-listing.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require_once $_tests_dir . '/includes/bootstrap.php';
-
-require 'assert-html.php';
