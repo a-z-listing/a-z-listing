2703850e30534ea67b2b8098deffade58da00cec
Daniel Llewellyn <daniel@bowlhat.net>
2019-05-16 17:52:16 +0100

    Revert PHPUnit version requirements
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,11 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
-require_once $_tests_dir . '/includes/bootstrap.php';
-
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/a-z-listing.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require_once $_tests_dir . '/includes/bootstrap.php';
 
 require 'assert-html.php';
