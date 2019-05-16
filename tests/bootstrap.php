565a8f546f6c1f469cdcdcb3f885878f27bad380
Daniel Llewellyn <daniel@bowlhat.net>
2019-05-16 17:11:31 +0100

    Attempt to appease new PHPUnit versions
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,11 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
-require_once $_tests_dir . '/includes/functions.php';
+require_once $_tests_dir . '/includes/bootstrap.php';
 
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/a-z-listing.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php';
 require 'assert-html.php';
