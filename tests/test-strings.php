49d0bba8096b9eda9b5cf4f3cfc1d23d564a46ab
Daniel Llewellyn <daniel@bowlhat.net>
2020-02-10 20:52:54 +0000

    Tests: Fix test_mb_string_to_array - incorrect method invokation
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- tests/test-strings.php
+++ tests/test-strings.php
@@ -4,7 +4,7 @@ class AZ_String_Tests extends WP_UnitTestCase {
     public function test_mb_string_to_array() {
         $input = "é€á";
         $expected = ["\xc3\xa9","\xe2\x82\xac","\xc3\xa1"];
-        $this->assertSame( $expected, \A_Z_Listing\Strings::mb_string_to_array() );
+        $this->assertSame( $expected, \A_Z_Listing\Strings::mb_string_to_array( $input ) );
     }
 
     public function test_maybe_mb_substr() {
