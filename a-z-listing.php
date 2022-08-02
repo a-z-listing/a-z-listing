bb7bbfba5fb1d80cbf9235df04ae28c540e22b28
Dani Llewellyn <dani@bowlhat.net>
2022-08-02 18:28:52 +0100

    Update a-z-listing.php
    Bump version number

--- a-z-listing.php
+++ a-z-listing.php
@@ -7,7 +7,7 @@
  * Author URI:      https://bowlhat.net/
  * Text Domain:     a-z-listing
  * Domain Path:     /languages
- * Version:         4.3.0
+ * Version:         4.3.1
  *
  * @package         A_Z_Listing
  */
